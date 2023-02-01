3a9a734fceecc3a7791e6670bf2f25aecc7d7e89
johnathansantos <johdougss@gmail.com>
2023-02-01 09:07:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ultravel-assinatura-email
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-z53c3s)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <div>Bernardo Pereira</div>
+    <div>gerente comercial</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  background-color: red;
+}
+</style>
